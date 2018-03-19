75d2109ab46d7b34e952cab23d4b601630714a26
SSK001 <santhoshksuram@gmail.com>
2018-03-19 15:42:18 -0400

    changes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='dsalgorithm',
-    version='1.0.0',
-    packages=[''],
+    version='1.0.1',
+    packages=['dsalgorithm'],
     url='https://github.com/SSK0001/Algorithms-DS',
     license='MIT License',
     author='Santhosh ',
